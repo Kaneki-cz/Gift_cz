81d58515b1fbe659bdd35c5e00e3bac24515605e
Kaneki_cz <dark4567054@gmail.com>
2025-08-28 15:04:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -256,7 +256,7 @@ class EncryptedMessage {
 
     randomizeText() {
         let randomizeCount = 0;
-        const maxRandomizations = 30;
+        const maxRandomizations = 10;
 
         const randomizeInterval = setInterval(() => {
             if (randomizeCount >= maxRandomizations) {
